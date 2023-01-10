9d2385493e0d611aa08b1fd393d0265d0b9e5d29
Christopher Hillman <40769598+CRHillman@users.noreply.github.com>
2023-01-10 21:13:01 +0000

    formatting

--- main.py
+++ main.py
@@ -11,39 +11,39 @@ intents.messages = True
 intents.message_content = True
 
 # create the bot
-client = discord.Client(intents=intents)
+client = discord.Client( intents=intents )
 
 
 # when it logs on
 @client.event
 async def on_ready():
-  print(f'{client.user} is now online!')
+  print( f'{client.user} is now online!' )
 
 
 # when a message appears
 @client.event
-async def on_message(message):
-  # print(message.content)
+async def on_message( message ):
+  # print( message.content )
 
   # to break the loop
-  if (message.author == client.user):
+  if ( message.author == client.user ):
     return
 
-  if (message.content.startswith('!')):
-    spell = clean(message.content).lower()
+  if ( message.content.startswith( '!' ) ):
+    spell = clean( message.content ).lower()
 
     try:
       wording = blairs[spell]
       # break into message-sized pieces
-      parts = (len(wording) // 2000) + 1
+      parts = ( len( wording ) // 2000 ) + 1
 
       # print spell name
-      await message.channel.send('***' + message.content[1:].upper() + '***')
+      await message.channel.send( '***' + message.content[1:].upper() + '***' )
 
       # send all the pieces
-      for i in range(parts):
+      for i in range( parts ):
         try:
-          await message.channel.send(wording[(i * 2000):(i + 1) * 2000])
+          await message.channel.send( wording[( i * 2000 ):( i + 1 ) * 2000] )
 
         except:
           pass
@@ -54,60 +54,60 @@ async def on_message(message):
 
     site = 'http://dnd5e.wikidot.com/spell:' + spell
 
-    #await message.channel.send( site )
+    #await message.channel.send(  site  )
 
-    response = requests.get(site)
+    response = requests.get( site )
 
     phrase = '<div class="content-separator" style="display: none:"></div>'
 
-    wording_start = response.text.find(phrase) - 1
+    wording_start = response.text.find( phrase ) - 1
 
-    wording_end = response.text.find(phrase, wording_start + len(phrase) + 1)
+    wording_end = response.text.find( phrase, wording_start + len( phrase ) + 1 )
 
-    #print( "Wording start: ", wording_start )
-    #print( "Wording end: ", wording_end )
+    #print(  "Wording start: ", wording_start  )
+    #print(  "Wording end: ", wording_end  )
 
-    wording = add_styling(response.text[wording_start:wording_end])
+    wording = add_styling( response.text[wording_start:wording_end] )
 
-    wording = remove_html(wording)
+    wording = remove_html( wording )
 
     # break into message-sized pieces
-    parts = (len(wording) // 2000) + 1
+    parts = ( len( wording ) // 2000 ) + 1
 
     # if there is no spell to find
-    if (len(wording) == 1):
-      await message.channel.send('No spell named: "' + message.content[1:] +
-                                 '" found.')
+    if ( len( wording ) == 1 ):
+      await message.channel.send( 'No spell named: "' + message.content[1:] +
+                                 '" found.' )
 
     else:
       # print the spell name
-      await message.channel.send('***' + message.content[1:].upper() + '***')
-      for i in range(parts):
+      await message.channel.send( '***' + message.content[1:].upper() + '***' )
+      for i in range( parts ):
         # send all the pieces
         try:
-          await message.channel.send(wording[(i * 2000):(i + 1) * 2000])
+          await message.channel.send( wording[( i * 2000 ):( i + 1 ) * 2000] )
 
         except:
           pass
 
 
-def clean(string):
+def clean( string ):
   '''
   Converts spaces, dashes, and slashes into hyphens
   '''
   cleaned = str()
 
   for char in string:
-    if (char in ' -/'):
+    if ( char in ' -/' ):
       cleaned += '-'
 
-    if (char.isalpha()):
-      cleaned += str(char)
+    if ( char.isalpha() ):
+      cleaned += str( char )
 
   return cleaned
 
 
-def remove_html(source):
+def remove_html( source ):
 
   ignore = False
   multi_space = False
@@ -116,19 +116,19 @@ def remove_html(source):
 
   for char in source:
     # ignore tags
-    if (char in '<'):
+    if ( char in '<' ):
       ignore = True
 
     # read other things
     if not ignore:
       # ampersands are weird
-      if (char in '&'):
+      if ( char in '&' ):
         wording += '\n'
         ignore = True
 
       else:
         # avoid wasting characters on spaces
-        if (char in ' ') and (multi_space):
+        if ( char in ' ' ) and ( multi_space ):
           pass
 
         else:
@@ -136,27 +136,31 @@ def remove_html(source):
           multi_space = False
 
     # stop ignoring because tag ended
-    if (char in '>;'):
+    if ( char in '>;' ):
       ignore = False
 
     # ignore multiple spaces
-    elif (char in ' '):
+    elif ( char in ' ' ):
       multi_space = True
 
   return wording
 
 
-def add_styling(source):
+def add_styling( source ):
 
-  replacements = [("<strong>", '**'), ("</strong>", '**'), ("&quot;", '"')]
+  replacements = [
+    ( "<strong>", '**' ), 
+    ( "</strong>", '**' ), 
+    ( "&quot;", '"' )
+  ]
 
   styled = source
 
   for old, new in replacements:
-    styled = styled.replace(old, new)
+    styled = styled.replace( old, new )
 
   return styled
 
 
 # always needed at the end
-client.run(TOKEN)
+client.run( TOKEN )
